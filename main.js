e0ae6e837f231422ad5dcf810eb7e0a2fb5b7e1c
Devik100 <74122137+Devik100@users.noreply.github.com>
2022-11-24 07:51:16 -0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,13 @@
+function preload() {
+    // how bout no
+}
+function setup() {
+    var canva = createCanvas(400, 400)
+    canva.center()
+}
+function draw() {
+    // not yet...
+}
+function yeetintofolder() {
+    save("Jevil.png")
+}
