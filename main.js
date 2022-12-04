d58a58fd25d9cbc469938a6b9a0b22b320ad3dca
Devik100 <74122137+Devik100@users.noreply.github.com>
2022-12-04 08:17:23 -0800

    Add files via upload

--- main.js
+++ main.js
@@ -2,12 +2,27 @@ function preload() {
     // how bout no
 }
 function setup() {
-    var canva = createCanvas(400, 400)
+    var canva = createCanvas(400, 300)
     canva.center()
+    video = createCapture(VIDEO)
+    video.size(400, 300)
+    video.hide()
+    poseNet=ml5.poseNet(video, modelLoaded)
+    poseNet.on("pose", gotPoses)
+}
+function modelLoaded() {
+    console.log("AS is ready to be an artificial stupidity")
+}
+function gotPoses(results) {
+    if (results.length > 0) {
+        console.log(results)
+        console.log("nosex = " + results[0].pose.nose.x)
+        console.log("nosey = " + results[0].pose.nose.y)
+    }
 }
 function draw() {
-    // not yet...
+    image(video, 0, 0, 400, 300)
 }
 function yeetintofolder() {
-    save("Jevil.png")
+    save("getareamustachefool.png")
 }
